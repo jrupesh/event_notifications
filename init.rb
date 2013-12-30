23089c792f37bd30d4cb066e78eeba1cd90c6a3c
Rupesh J <j.rupesh@gmail.com>
2013-12-30 12:02:52 +0530

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :event_notifications do
+  name 'Event Notifications plugin'
+  author 'Rupesh J'
+  description 'Customizes redmine project notification settings for every project event.'
+  version '0.0.1'
+  #url 'http://example.com/path/to/plugin'
+  author_url 'mailto:rupeshj@esi-group.com'
+end
