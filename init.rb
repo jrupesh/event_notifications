00843e69699e4af0fe9072e8e49da0bb337a6d22
Rupesh J <j.rupesh@gmail.com>
2015-05-28 15:05:48 +0530

    Version update.

--- init.rb
+++ init.rb
@@ -24,7 +24,7 @@ Redmine::Plugin.register :event_notifications do
   name 'Event Notifications plugin'
   author 'Rupesh J'
   description 'Customizes redmine project notification settings for every project event.'
-  version '2.0.0'
+  version '2.0.1'
   author_url 'mailto:rupeshj@esi-group.com'
 
   settings :default => {
