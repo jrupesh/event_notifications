5bbc18134b58b6f88662d793f30220d480e7664b
ESI Redmine <redmine_agent@esi-group.com>
2015-05-28 11:27:17 +0200

    rake task update and Message bug updated.

--- lib/event_notification/patches/message_patch.rb
+++ lib/event_notification/patches/message_patch.rb
@@ -7,19 +7,19 @@ module EventNotification
 
         base.class_eval do
           unloadable
-          alias_method_chain :recipients, :events
+          alias_method_chain :notified_users, :events
         end
       end
 
       module InstanceMethods
-        def recipients_with_events
+        def notified_users_with_events
           return [] if User.current.ghost?
           if Setting.plugin_event_notifications["enable_event_notifications"] == "on"
             notified = project.notified_users(self)
             notified.reject! {|user| !visible?(user)}
             notified.collect(&:mail)
           else
-            recipients_without_events
+            notified_users_without_events
           end
         end
       end
